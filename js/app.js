8612f2669d9308bde189510632725a81145a2785
Ritik Sharma <sharmaritik2002@gmail.com>
2023-09-02 22:35:42 +0530

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 const getWeather = async() =>{
     
     const weatherOutput = document.getElementById('results')
@@ -11,29 +12,27 @@ const getWeather = async() =>{
         var cities = input.split(',')
         cities = cities.map(city => city.trim())
 
-        const res = await fetch('http://localhost:5000/getWeather', {
-            method: "POST",
-            headers:{
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify({"cities": cities})
-        })
-        console.log(res);
-        if (res.status == 200) {
-            const data = await res.json()
+        // const res = await fetch('http://localhost:5000/getWeather', {
+        //     method: "POST",
+        //     headers:{
+        //         "Content-Type": 'application/json'
+        //     },
+        //     body: JSON.stringify({"cities": cities})
+        // })
 
-            for (const city in data.Weather) {
-                const temp = data.Weather[city]
-                const cityTemp = document.createElement('li')
-                cityTemp.textContent = `${city}: ${temp}`
-                weatherOutput.appendChild(cityTemp)
-            }
+        const weather = {}
+        for(const city of cities){
+            weather[city] = await getTemperature(city)
+            console.log(weather);
         }
-        else{
-            const output = document.createElement('li')
-            output.textContent = res.text
-            weatherOutput.appendChild(output)
+
+        for (const city in weather) {
+            const temp = weather[city]
+            const cityTemp = document.createElement('li')
+            cityTemp.textContent = `${city}: ${temp}`
+            weatherOutput.appendChild(cityTemp)
         }
+
     }catch(error){
 
         const output = document.createElement('li')
@@ -41,4 +40,25 @@ const getWeather = async() =>{
         weatherOutput.appendChild(output)
     }
 
+}
+
+
+async function getTemperature(city){
+    try{
+        const API_KEY = "b8ac4999c21240194810d8e33caab140"
+        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+        const res = await fetch(API_URL, {
+            method: "GET"
+        })
+        const data = await res.json()
+
+        // const res = await axios.get(API_URL);
+
+        return `${data.main.temp} \u00B0C`
+
+    }catch(error){
+        console.log("Error: ",error);
+        return "error"
+    }
 }
